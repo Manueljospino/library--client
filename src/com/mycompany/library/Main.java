265e31211b58eeca21efe594849e64a47be49f8c
Manueljospino <promanu98@gmail.com>
2026-08-16 11:00:01 -0500

    feat: implement CREATE for Book

--- src/com/mycompany/library/Main.java
+++ src/com/mycompany/library/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     static ArrayList<Customer> customers = new ArrayList<>();
     static Scanner enter = new Scanner(System.in);
+    static ArrayList<Book> books = new ArrayList<>();
 
   public static void createCustomer (){
       System.out.println("====Registro de clientes=====");
@@ -17,7 +18,7 @@ public class Main {
       String name = enter.nextLine();
       System.out.print("Teléfono :  ");
       String phone = enter.nextLine();
-      System.out.println("Correo:   ");
+      System.out.print("Correo:   ");
       String email = enter.nextLine();
 
       Customer c = new  Customer(id,name,phone,email);
@@ -90,6 +91,39 @@ public class Main {
          System.out.println("cliente eliminado exitosamente!");
 
      }
+
+
+    public static void createBook () {
+        System.out.println("====Registro de libros=====");
+        System.out.println();
+        System.out.print("Código                     :  ");
+        String code = enter.nextLine();
+        System.out.print("Título                     :  ");
+        String title = enter.nextLine();
+        System.out.print("Año en el que se publicó   :  ");
+        int publicationYear = enter.nextInt();
+        enter.nextLine();
+        System.out.print("Autor                      :   ");
+        String author = enter.nextLine();
+        String availableInput;
+        do {
+            System.out.println("Disponible si o no? (S/N): ");
+            availableInput = enter.nextLine().toUpperCase();
+            if (!availableInput.equals("S") && !availableInput.equals("N")) {
+                System.out.println("Respuesta inválida. Escriba S o N.");
+            }
+
+
+        } while (!availableInput.equals("S") && !availableInput.equals("N"));
+        boolean available = availableInput.equals("S");
+
+        Book b = new Book(code, title, publicationYear, author, available);
+        books.add(b);
+        System.out.println("Libro registrado exitosamente!");
+        System.out.println();
+
+    }
+
 
     public static void main(String[] args) {
 
